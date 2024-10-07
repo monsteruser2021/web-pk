c451ade5193119f09847bd8d7179669412975025
monsteruser2021 <monsteruser2021@gmail.com>
2024-10-07 13:14:05 -0400

    paleta

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#05d3f8',
+        secondary: '#009ddd',
+        tertiary: '#386dbd',
+        quaternary: '#0f4571',
+        quinary: '#3a3132',
+      },
+    },
   },
   plugins: [],
 }
